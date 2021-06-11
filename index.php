fafd9731176ab3120ad138dff9c67361517fdf36
Martin SION <martin@sionfamily.com>
2021-06-11 21:09:54 +0200

    Change and english txt

--- index.php
+++ index.php
@@ -10,25 +10,22 @@
     </section>
 
     <section class="section2">
-
+        <h2>In 1993, ID Software releases Doom which becomes an international success. One year after its release, a website by the name of LMP Hall of Fame 
+            lets players upload their gameplay clips in the hopes of obtaining “DOOM Honorifics Titles”, the goal being finishing the game with various challenges or as quick as possible. This is 
+            how speedrunning was born. In November of the same year the COMPET-N website was created and becomes the cornerstone of the DOOM speedruning community. </h2>
     </section>
     <section class="section3">
-        <h2 class="demo">Dans les années qui suivent cette pratique se démocratise sur de multiples jeux et l’apparition du site Speed Demo Archive 
-        deviens la référence du speedrunning pour mettre en avant ses prouesses</h2>
-        <h2 class="twitch">L’ouverture de twitch en 2011 offre aux speedrunners un nouveau media afin de permettre de partager leur contenu, en effet 
-        le principe du live permet aux spectateur de voir la “run” en direct et d’interragir avec le speedrunner via le chat</h2>
+        <h2 class="demo">In the following years, speedrunning became common practice with multiple other games. The Speed Demo website then becomes the reference in terms of speedrunning content.</h2>
+        <h2 class="twitch">Twitch’s arrrival onto  the scene in 2011 offers speedrunners a new platform where they can share their content and exploits; live-streaming allows spectators to watch the run live and interact with the speedrunner via chat.</h2>
     </section>
     <section class="section4">
-        <img src="./assets/img_event/CharityStream_April2021_1920px 1.png" alt="brawlhalla">
-        <img src="./assets/img_event/image_76.png" alt="livetime">
-        <img src="./assets/img_event/image_77.png" alt="bourglarun">
-        <h2 class="event">Avec la montée en popularité des GDQ de plus en plus de personnes se sont intéréssé aux stream cartitatifs allant 
-        jusqu’a organiser leur propre évenement sans  forcément entrer dans la pratique du speedrun</h2>
-        <img src="./assets/img_event/image_79.png" alt="msf">
-        <img src="./assets/img_event/image_80.png" alt="speedrun4life">
-        <img src="./assets/img_event/image_81.png" alt="lfr">
-
-    </section>
+        <img src="./assets/img/brawlhalla.png" alt="brawlhalla">
+        <img src="./assets/img/livetime.png" alt="livetime">
+        <img src="./assets/img/bourglarun.png" alt="bourglarun">
+        <h2 class="event">Following speedrunning rise to popularity, people started to organise charity events around this phenomenon. That’s how the very first edition of Awesome Games Done Quick came to be in 2010.</h2>
+        <img src="./assets/img/mariomasterscolosseum.png" alt="msf">
+        <img src="./assets/img/speedrun4life.png" alt="speedrun4life">
+        <img src="./assets/img/lfr@ga18.png" alt="lfr">
     </section>
 
 </main>
