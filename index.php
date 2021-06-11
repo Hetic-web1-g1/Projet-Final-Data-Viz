3f48913ced2fdd2ddb232bf3a2445ef2af2a7c9e
Spudwebb96 <jonas.janko17@gmail.com>
2021-06-11 14:15:13 +0200

    section 4 debut

--- index.php
+++ index.php
@@ -14,11 +14,16 @@
 
     </section>
     <section class="section3">
-    <h2 class="demo">Dans les années qui suivent cette pratique se démocratise sur de multiples jeux et l’apparition du site Speed Demo Archive 
-    deviens la référence du speedrunning pour mettre en avant ses prouesses</h2>
-    <h2 class="twitch">L’ouverture de twitch en 2011 offre aux speedrunners un nouveau media afin de permettre de partager leur contenu, en effet 
-    le principe du live permet aux spectateur de voir la “run” en direct et d’interragir avec le speedrunner via le chat</h2>
-    <img src="./assets/Glitch_Purple_RGB.png" alt="twitch" width="32px">
+        <h2 class="demo">Dans les années qui suivent cette pratique se démocratise sur de multiples jeux et l’apparition du site Speed Demo Archive 
+        deviens la référence du speedrunning pour mettre en avant ses prouesses</h2>
+        <h2 class="twitch">L’ouverture de twitch en 2011 offre aux speedrunners un nouveau media afin de permettre de partager leur contenu, en effet 
+        le principe du live permet aux spectateur de voir la “run” en direct et d’interragir avec le speedrunner via le chat</h2>
+        <img src="./assets/Glitch_Purple_RGB.png" alt="twitch" width="32px">
+    </section>
+    <section class="section4">
+        <h2 class="event">Avec la montée en popularité des GDQ de plus en plus de personnes se sont intéréssé aux stream cartitatifs allant 
+        jusqu’a organiser leur propre évenement sans  forcément entrer dans la pratique du speedrun</h2>
+
     </section>
 
 </main>
