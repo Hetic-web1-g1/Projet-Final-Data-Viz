f94b6a0805b40bf7841561139d8190dc7cc34af4
Spudwebb96 <jonas.janko17@gmail.com>
2021-06-12 19:47:51 +0200

    fix orthographe ( merci martin)

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 ?>
 
 <main>
-    <section id="acceuil">
+    <section id="accueil">
         <h1 id="speed"><span>Speed</span><span>Run</span></h1>
         <a href="#video"><img src="./assets/img/arrow.svg" alt="bouton fleche"></a>
     </section>
