bdc4928ffc3aefd8fc4aab29368971d78d38e9ab
Martin SION <martin@sionfamily.com>
2021-06-12 17:25:05 +0200

    change

--- index.php
+++ index.php
@@ -18,7 +18,7 @@
     <section id="introtwitch">
         <p class="demo">In the following years, speedrunning became common practice with multiple other games. The Speed Demo website then becomes the reference in terms of speedrunning content.</p>
         <p class="twitch">Twitch’s arrrival onto  the scene in 2011 offers speedrunners a new platform where they can share their content and exploits; live-streaming allows spectators to watch the run live and interact with the speedrunner via chat.</p>
-    </section>t
+    </section>
     <section id="stat_gdq">
         <svg id='bar_chart'></svg>
         <script src="js/bar chart.js"></script>
