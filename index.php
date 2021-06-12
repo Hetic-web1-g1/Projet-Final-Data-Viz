b9171a4c8e711c4b8b13fba44d6704ec12f5f753
Erzenkel <ferrier.sammy@gmail.com>
2021-06-12 16:22:51 +0200

    remplaçage class par id

--- index.php
+++ index.php
@@ -4,23 +4,23 @@
 ?>
 
 <main>
-    <section class="section1">
+    <section id="section1">
         <h1 id="speed"><span>Speed</span><span>Run</span></h1>
         <!-- <script src="js/section1.js"></script> -->
     </section>
     <section class="video">
         <video src="./assets/video/speedrun.mp4" autoplay loop muted></video>
     </section>
-    <section class="section2">
+    <section id="section2">
         <h2>In 1993, ID Software releases Doom which becomes an international success. One year after its release, a website by the name of LMP Hall of Fame 
             lets players upload their gameplay clips in the hopes of obtaining “DOOM Honorifics Titles”, the goal being finishing the game with various challenges or as quick as possible. This is 
             how speedrunning was born. In November of the same year the COMPET-N website was created and becomes the cornerstone of the DOOM speedruning community. </h2>
     </section>
-    <section class="section3">
+    <section id="section3">
         <h2 class="demo">In the following years, speedrunning became common practice with multiple other games. The Speed Demo website then becomes the reference in terms of speedrunning content.</h2>
         <h2 class="twitch">Twitch’s arrrival onto  the scene in 2011 offers speedrunners a new platform where they can share their content and exploits; live-streaming allows spectators to watch the run live and interact with the speedrunner via chat.</h2>
     </section>
-    <section class="section4">
+    <section id="section4">
         <img src="./assets/img/speedons.svg" alt="speedons" tag-inviewport="scale-in">
         <img src="./assets/img/livetime.svg" alt="livetime" tag-inviewport="scale-in">
         <img src="./assets/img/bourglarun.svg" alt="bourglarun" tag-inviewport="scale-in">
@@ -29,7 +29,7 @@
         <img src="./assets/img/lefrenchrestream.svg" alt="lefrenchrestream" tag-inviewport="scale-in">
         <script src="js/section4.js"></script>
     </section>
-    <section class="section5" style="margin: 30%;">
+    <section id="section5" style="margin: 30%;">
         <h1>Loreum Ipsum</h1>
         <h2>Loreum Ipsum</h2>
         <h3>Loreum Ipsum</h3>
@@ -39,6 +39,9 @@
         <p>Loreum Ipsum</p>
         <span>Loreum Ipsum</span>
         <a href="">Loreum Ipsum</a>
+    </section>
+    <section id="pie chart">
+
     </section>
 
 </main>
