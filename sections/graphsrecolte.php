c39580ee5c87edb467589a13d274ecce73793c7b
Jérémie <jeremie.jourda202@gmail.com>
2021-06-14 01:42:29 +0200

    Prettry print tout joli

--- sections/graphsrecolte.php
+++ sections/graphsrecolte.php
@@ -1,6 +1,6 @@
 <section id="graphsrecolte">
     <div class='bouton'>
-            <button onclick="update_chart(zevent,'Récolte Zevent', 'graphsrecolte')">récoltes Zevent</button>
-            <button onclick="update_chart(pic_zevent,'Pic spectateurs Zevent', 'graphsrecolte')">Pic viewer Zevent</button>
+        <button onclick="update_chart(zevent,'Récolte Zevent', 'graphsrecolte')">récoltes Zevent</button>
+        <button onclick="update_chart(pic_zevent,'Pic spectateurs Zevent', 'graphsrecolte')">Pic viewer Zevent</button>
     </div>
 </section>
--- sections/sondage.php
+++ sections/sondage.php
@@ -1,6 +1,6 @@
 <section id="sondage">
     <h3>Es-ce que vous regarez des speedruns ou autres streamns a des fins caritatifs ?</h3>
-    <div id = 'age_pie'></div>
+    <div id='age_pie'></div>
     <h3>Avez-vous deja regarde le zevent ou des evenements de ce genre ?</h3>
     <p>Graph</p>
     <h3>Irez-vous voir un strea caritatif s’il y a un streamer que vous aimez ?</h3>
