242fc8c375dd65964ed669483ef63ecaa61f8e02
Martin SION <martin@sionfamily.com>
2021-06-13 03:00:37 +0200

    save3

---
+++ section/sondage.php
@@ -0,0 +1,6 @@
+<section id="sondage">
+    <h3>D’apres un sondage aupres de la populace entre 15 et 30 ans</h3>
+    <p>Es-ce que vous regarez des s[eedruns ou atres streamns a des fins caritatifs ?</p>
+    <p>Avez-vous deja regarde le zevent ou des evenements de ce genre ?</p>
+    <p>Irez-vous voir un strea caritatif s’il y a un streamer que vous aimez ?</p>
+</section>
