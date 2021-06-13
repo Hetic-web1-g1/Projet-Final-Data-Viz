49f2c55cb3294a11024f4d6448e5de522cbd9b3b
Spudwebb96 <jonas.janko17@gmail.com>
2021-06-13 20:43:04 +0200

    page solostreamers

--- sections/solostreams.php
+++ sections/solostreams.php
@@ -1,8 +1,30 @@
 <section id="solostreams">
-    <img src="./assets/img/coro.png" alt="coro">
-    <img src="./assets/img/benzai.png" alt="benzai">
-    <img src="./assets/img/remigaillard.png" alt="remigaillard">
-    <img src="./assets/img/random1.png" alt="random1">
-    <img src="./assets/img/random2.png" alt="random2">
-    <p>travailler contenu figma avant de mettre en forme</p>
+    <div id="boxstreams">
+        <figure>
+            <img src="./assets/img/coro.png" alt="coro">
+            <figcaption>Corobizar</figcaption>
+        </figure>
+        <span>Hall of fame</span>
+        <figure>
+            <img src="./assets/img/benzai.png" alt="benzai">
+            <figcaption>Benzai</figcaption>
+        </figure>
+        <figure>
+            <img src="./assets/img/random2.png" alt="random2">
+            <figcaption>Mew2King</figcaption>
+        </figure>
+        <figure>
+            <img src="./assets/img/random1.png" alt="random1">
+            <figcaption>ManuelleFerraraTV</figcaption>
+        </figure>
+        <figure>
+            <img src="./assets/img/remigaillard.png" alt="remigaillard">
+            <figcaption>Rémi Gallaird</figcaption>
+        </figure>
+    </div>
+
+        <p>
+            Voici une liste de streameur qui à participé sans forcément créée d’évenement communautaire à l’envoie de don pour les associations
+        </p>
+                
 </section>
