74902709ab46113f99195ff347810cbdc06a512b
Martin SION <martin@sionfamily.com>
2021-06-14 05:02:48 +0200

    fixassociation

--- sections/sondage.php
+++ sections/sondage.php
@@ -1,4 +1,5 @@
 <section id="sondage">
     <h3>Es-ce que vous regarez des speedruns ou autres streamns a des fins caritatifs ?</h3>
     <div id = 'age_pie'></div>
+    <p>faire la p2 sondage</p>
 </section>
