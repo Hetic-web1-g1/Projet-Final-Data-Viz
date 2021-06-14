fe2f1be7a5b3a405ce9c367bd2aa797845a7d6c1
Martin SION <martin@sionfamily.com>
2021-06-14 14:23:15 +0200

    traduction

--- sections/associations.php
+++ sections/associations.php
@@ -2,7 +2,7 @@
         <a id="spa" href="https://www.la-spa.fr/nos-missions" target="_blank"><img src="./assets/img/spa.svg" alt="" tag-inviewport="scale-in"></a>
         <a id="croixrouge" href="https://paris.croix-rouge.fr/paris-1er-2e" target="_blank"><img src="./assets/img/croixrouge.svg" alt="" tag-inviewport="scale-in"></a>
         <a id="medecins" href="https://www.msf.fr/agir/soutenir-nos-actions/faire-un-don-en-ligne?cid=87&reserved_field=W**W00001&esv_source=Google&esv_medium=SEA_Brand&esv_campaign=W**W00001&reserved_tracking_iraiser=adwords&esv_term=m%C3%A9decins%20sans%20fronti%C3%A8res&gclid=CjwKCAjwtpGGBhBJEiwAyRZX2rL6ve_74sfnH0KQErrgRNTlQJNPVanLcCvYsyC_NHwcSZEMsBytehoCGCgQAvD_BwE" target="_blank"><img src="./assets/img/medecinssf.svg" alt="" tag-inviewport="scale-in"></a>
-        <p>Mais aussi des millions d’euro récolté pour aider ces associations à but non lucratives.</p>
+        <p>But also millions of euros collected to help these non-profit organizations.</p>
         <a id="savethechildren" href="https://donate.savethechildren.org/en?utm_source=google&utm_medium=cpc&utm_campaign=gsn%20west%20brand%20care%20exact&utm_term=save%20the%20children&gclid=CjwKCAjwtpGGBhBJEiwAyRZX2hyY8OEnTFSnAAYdL9oDscXNXu3wjciHn7pehgEGYkz5RfsL7TcY4BoCwYUQAvD_BwE" target="_blank"><img src="./assets/img/savetc.svg" alt="" tag-inviewport="scale-in"></a>
         <a id="institutpasteur" href="https://www.pasteur.fr/fr" target="_blank"><img src="./assets/img/institutpasteur.svg" alt="" tag-inviewport="scale-in"></a>
         <a id="preventcancer" href="https://www.preventcancer.org/" target="_blank"><img src="./assets/img/preventcancer.svg" alt="" tag-inviewport="scale-in"></a>
