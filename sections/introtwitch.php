4c6411eea5888fc793140dc48ce86a4853a3b39b
Martin SION <martin@sionfamily.com>
2021-06-14 17:47:43 +0200

    change solostream

--- sections/introtwitch.php
+++ sections/introtwitch.php
@@ -1,9 +1,10 @@
 <section id="introtwitch">
-    <div class="flex"></div>
-    <h3>SPEEDRUN AND TWITCH</h3>
-    <p>In the following years, speedrunning became common practice with multiple other games. The Speed Demo
-        website then becomes the reference in terms of speedrunning content.</p>
-    <p id="twitch">Twitch’s arrrival onto the scene in 2011 offers speedrunners a new platform where they can share
-        their content and exploits; live-streaming allows spectators to watch the run live and interact with the speedrunner
-        via chat.</p>
+    <div class="flex">
+        <h3>SPEEDRUN AND TWITCH</h3>
+        <p>In the following years, speedrunning became common practice with multiple other games. The Speed Demo
+            website then becomes the reference in terms of speedrunning content.</p>
+        <p id="twitch">Twitch’s arrrival onto the scene in 2011 offers speedrunners a new platform where they can share
+            their content and exploits; live-streaming allows spectators to watch the run live and interact with the speedrunner
+            via chat.</p>
+    </div>
 </section>
--- sections/solostreams.php
+++ sections/solostreams.php
@@ -1,28 +1,48 @@
-<section id="solostreams">
-    <div id="boxstreams">
-        <figure>
-            <img src="./assets/img/coro.png" alt="coro" >
-            <figcaption>Corobizar</figcaption>
-        </figure>
-        <h3>STREAMING BECOMES MAINSTREAM</h3>
-        <figure>
-            <img src="./assets/img/benzai.png" alt="benzai">
-            <figcaption>Benzai</figcaption>
-        </figure>
-        <figure>
-            <img id ="m2k" src="./assets/img/mew2king.png" alt="mew2king">
-            <figcaption>Mew2King</figcaption>
-        </figure>
-        <figure>
-            <img src="./assets/img/manuelferrara.jpg" alt="manuelferrara">
-            <figcaption>ManuelleFerraraTV</figcaption>
-        </figure>
-        <figure>
-            <img src="./assets/img/remigaillard.png" alt="remigaillard">
-            <figcaption>Rémi Gaillard</figcaption>
-        </figure>
+<section id="solostreams" class="flexalt">
+    <h3>STREAMING BECOMES MAINSTREAM</h3>
+    <div>
+        <div class="flexalt">
+            <img src="./assets/img/grandpoobear.jpg" alt="Grand Poo Bar">
+            <figcaption>Grand Poo Bar</figcaption>
+            <p>In 2019, raised $186,771 for Direct Relief.</p>
+        </div>
+        <div class="flexalt">
+            <img src="./assets/img/drlupo.jpg" alt="DrLupo">
+            <figcaption>DrLupo</figcaption>
+            <p>In 2020, raised $2,339,943 for St. Jude Research Hospital during a 24h charity stream.</p>
+        </div>
+        <div class="flexalt">
+            <img src="./assets/img/ninja.jpg" alt="Ninja">
+            <figcaption>Ninja</figcaption>
+            <p>In 20018, raised $121,000 against sucide.</p>
+        </div>
     </div>
-
-    <p class="flex">Here's a list of streamers that were able to make charitable contributions via their own solo streaming event which shows how far a single dedicated fanbase can go in terms of potential donations.</p>
-
+    <p class="flexalt">With streaming rise to fame in mainstream media, streamers started their own solo charity events (right from their home). They are now able to raise significant funds which shows how far a single dedicated fanbase can go in terms of contributions to a given charity.</p>
 </section>
+
+
+<!-- <div id="boxstreams">
+    <figure>
+        <img src="./assets/img/coro.png" alt="coro" >
+        <figcaption>Corobizar</figcaption>
+    </figure>
+
+    <figure>
+        <img src="./assets/img/benzai.png" alt="benzai">
+        <figcaption>Benzai</figcaption>
+    </figure>
+    <figure>
+        <img id ="m2k" src="./assets/img/mew2king.png" alt="mew2king">
+        <figcaption>Mew2King</figcaption>
+    </figure>
+    <figure>
+        <img src="./assets/img/manuelferrara.jpg" alt="manuelferrara">
+        <figcaption>ManuelleFerraraTV</figcaption>
+    </figure>
+    <figure>
+        <img src="./assets/img/remigaillard.png" alt="remigaillard">
+        <figcaption>Rémi Gaillard</figcaption>
+    </figure>
+</div>
+
+<p class="flex">Here's a list of streamers that were able to make charitable contributions via their own solo streaming event which shows how far a single dedicated fanbase can go in terms of potential donations.</p> -->
