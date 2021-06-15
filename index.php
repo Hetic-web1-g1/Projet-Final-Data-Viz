205baaf73846c54de0e9bce1abd4a715d2afda9e
Martin SION <martin@sionfamily.com>
2021-06-15 11:05:13 +0200

    cleaning

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ include './header.php';
 
 <main onscroll="verifScroll()">
     <?php
-    include './sections/accueil.php';
+    include './sections/introduction.php';
     include './sections/introspeedrun.php';
     include './sections/video.php';
     include './sections/introevent.php';
@@ -16,16 +16,6 @@ include './header.php';
     include './sections/twitchevo.php';
     include './sections/sondage.php';
     include './sections/associations.php';
-
-    // include './sections/speedrunningrise.php';
-    // include './sections/charitystreams.php';
-    
-    // include './sections/graphsrecolte.php';
-    
-    // include './sections/comparaison.php';
-    
-    
-    // include './sections/conclusion.php';
     ?>
 </main>
 
