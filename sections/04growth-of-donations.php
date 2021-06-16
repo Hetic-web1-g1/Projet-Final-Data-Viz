fc509d8a976c38c6950b2bd9cad3d5ac97f96bf3
Jérémie <jeremie.jourda202@gmail.com>
2021-06-16 11:44:06 +0200

    Ajouts de traits

--- sections/04growth-of-donations.php
+++ sections/04growth-of-donations.php
@@ -30,7 +30,7 @@
                         <p class="caption">Rise of donations durings Z-Events streams through years.</p>
                 </div>
                 <div class="left margin-bottom">
-                        <p>Z-event is a francophone charity project created in 2017 by Zerator. It gathers a group of streamers to rally for the same cause over a weekend. As of today it has raised more than any other charity streaming event. </p>
+                        <p class="not-linked">Z-event is a francophone charity project created in 2017 by Zerator. It gathers a group of streamers to rally for the same cause over a weekend. As of today it has raised more than any other charity streaming event. </p>
                 </div>
         </div>
 </section>
