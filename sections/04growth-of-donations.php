60e6d2d8e66107a2dc4f306f55f77f90fb4c0c89
Martin SION <martin@sionfamily.com>
2021-06-16 14:55:40 +0200

    fix space button

--- sections/04growth-of-donations.php
+++ sections/04growth-of-donations.php
@@ -24,10 +24,10 @@
                         <div class="coms">
                         <p>Z-event is a francophone charity project created in 2017 by Zerator. It gathers a group of streamers to rally for the same cause over a weekend. As of today it has raised more than any other charity streaming event.</p>
                         </div>
-                        <div class="buttons">
-                        <button class="button_zevent on" onclick="update_chart(zevent,'Statistics Z-Event', 'stat_z-event')"><h4>Donations</h4></button>
-                        <br>
-                        <button class="button_zevent" onclick="update_chart(pic_zevent,'Statistics Z-Event', 'stat_z-event')"><h4>Viewers</h4></button>
+                        <div class="buttons flex">
+                                <button class="button_zevent on" onclick="update_chart(zevent,'Statistics Z-Event', 'stat_z-event')"><h4>Donations</h4></button>
+                                <br>
+                                <button class="button_zevent" onclick="update_chart(pic_zevent,'Statistics Z-Event', 'stat_z-event')"><h4>Viewers</h4></button>
                         </div>
                         </div>
                 </div>
