a3117bafb85775418504bd80d3b90d720accb80d
FlyLeBro <alexandre912323@gmail.com>
2021-06-16 12:01:29 +0200

    rect

--- sections/08affiliated-charities.php
+++ sections/08affiliated-charities.php
@@ -19,6 +19,6 @@
                 <p>"Ce site a été réalisé à des fins pédagogiques dans le cadre du cursus Grande Ecole de l’école HETIC. Les 
                 contenus et données présentés n'ont pas fait l'objet d'une demande de droit d'utilisation. Ce site ne sera en 
                 aucun cas exploité à des fins commerciales”</p>
-                <p class="content-margins">Un grand merci à Alexandre 'Dach' Dachary pour les informations qu'il nous a fournis.</p>
+                <p class="content-margins">Un grand merci à Alexandre 'Dach' Dachary pour les informations qu'il nous a fournies.</p>
         </div>
 </section>
