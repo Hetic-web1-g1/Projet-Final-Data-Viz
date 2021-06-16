7d0f249017f2d58f3a165903d4f0b43c19f09a2e
Jérémie <jeremie.jourda202@gmail.com>
2021-06-16 12:36:41 +0200

    Add target blank

--- sections/08affiliated-charities.php
+++ sections/08affiliated-charities.php
@@ -21,6 +21,6 @@
                 <p>"Ce site a été réalisé à des fins pédagogiques dans le cadre du cursus Grande Ecole de l’école HETIC. Les 
                 contenus et données présentés n'ont pas fait l'objet d'une demande de droit d'utilisation. Ce site ne sera en 
                 aucun cas exploité à des fins commerciales”</p>
-                <p class="content-margins">Un grand merci à Alexandre <a href="https://twitter.com/alexdach">'Dach'</a> Dachary pour les informations qu'il nous a fournies.</p>
+                <p class="content-margins">Un grand merci à Alexandre <a href="https://twitter.com/alexdach" target="_blank">'Dach'</a> Dachary pour les informations qu'il nous a fournies.</p>
         </div>
 </section>
